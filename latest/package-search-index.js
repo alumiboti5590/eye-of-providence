42e37bb7171bd13d39f0392a13c87cdeb72e7b62
dstarner <dstarner@users.noreply.github.com>
2023-03-28 15:40:04 +0000

    deploy: eb079b209fb2330ad93c4320fcde197bfbd67008

--- latest/package-search-index.js
+++ latest/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.alumiboti5590.eop.pid"},{"l":"org.alumiboti5590.eop.utils"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.alumiboti5590.eop.controllers"},{"l":"org.alumiboti5590.eop.pid"},{"l":"org.alumiboti5590.eop.utils"}]
