83dc63f40740e91d9aba7949e7f21405b0670f77
dstarner <dstarner@users.noreply.github.com>
2023-03-28 17:41:27 +0000

    deploy: 27aed44ca171bde6dfa345d4968048cae9d142fe

---
+++ 0.1.0/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.alumiboti5590.eop.utils","l":"DistanceUtil"},{"p":"org.alumiboti5590.eop.pid","l":"PIDGains"},{"p":"org.alumiboti5590.eop.controllers","l":"XboxController"}]
