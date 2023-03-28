481ca533a10fb5bf3c6f2b8fe1f6c15ab3ef8224
Dan Starner <me@danstarner.com>
2023-03-27 22:06:38 -0400

    initial commit with PIDGains added

---
+++ README.md
@@ -0,0 +1,5 @@
+# Eye of Providence
+
+This is a collection of reusable code pieces for the Alumiboti to use in their
+robot projects. The goal is to reduce the amount of code that needs to be
+created or copied year-over-year.
---
+++ lib/src/main/java/org/alumiboti/eop/pid/PIDGains.java
@@ -0,0 +1,50 @@
+/* 2023 Written by Alumiboti FRC 5590 */
+package org.alumiboti.eop.pid;
+
+/**
+ * PIDGains allows for easily storing PID values in an object that can be pulled
+ * out and used against PID controllers. There are a slew of constructors but the
+ * values given are final; they cannot be changed.
+ */
+public class PIDGains {
+
+    public static final double DEFAULT_PEAK_OUTPUT = 1;
+
+    public double kP, kI, kD, kFF, kIzone, kPeakMinOutput, kPeakMaxOutput;
+
+    /**
+     * PIDGains constructor with only the P, I, and D values provided
+     */
+    public PIDGains(double p, double i, double d) {
+        this(p, i, d, 0, 0, DEFAULT_PEAK_OUTPUT);
+    }
+
+    /**
+     * PIDGains constructor with all values but a mirrored peak value. Pass a number
+     * 'x' and the peak outputs will be [-x, x]
+     */
+    public PIDGains(double p, double i, double d, double feedForward, double izone, double peakOutput) {
+        this(p, i, d, feedForward, izone, peakOutput == 0 ? 0 : -peakOutput, peakOutput);
+    }
+
+    /**
+     * PIDGains constructor with all values potentially defined, including
+     * non-symmetrical values for the peak minimum and maximum values.
+     */
+    public PIDGains(
+            double p,
+            double i,
+            double d,
+            double feedForward,
+            double izone,
+            double peakMinOutput,
+            double peakMaxOutput) {
+        this.kP = p;
+        this.kI = i;
+        this.kD = d;
+        this.kFF = feedForward;
+        this.kIzone = izone;
+        this.kPeakMinOutput = peakMinOutput;
+        this.kPeakMaxOutput = peakMaxOutput;
+    }
+}
