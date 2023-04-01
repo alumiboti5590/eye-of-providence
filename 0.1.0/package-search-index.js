8763d5be9ef9afcc27f2506ddb644e9adaef530c
Dan Starner <github@danstarner.com>
2023-04-01 12:07:20 -0400

    remove bad versions

--- 0.1.0/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.alumiboti5590.eop.controllers"},{"l":"org.alumiboti5590.eop.pid"},{"l":"org.alumiboti5590.eop.utils"}]
--- v0.0.1/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.alumiboti5590.eop.controllers"},{"l":"org.alumiboti5590.eop.pid"},{"l":"org.alumiboti5590.eop.subsystems.drivetrain.tank"},{"l":"org.alumiboti5590.eop.subsystems.util"},{"l":"org.alumiboti5590.eop.utils"}]
